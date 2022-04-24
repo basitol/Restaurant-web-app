b1018a8a618b0c868a6ecb11c64a6fd4d93d4440
basitol <41623944+basitol@users.noreply.github.com>
2022-04-24 15:53:13 +0100

    added: link structure to navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <>
+      <section className="flex flex-jc-sb">
+        <div>Restaurant</div>
+        <nav className="navbar close flex">
+          <ul className="flex">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/product">Product</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+      </section>
+    </>
+  );
 };
 
 export default Navbar;
