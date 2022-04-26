dc780df9856f5b2092c4d171419530e5fe4c2e9a
basitol <41623944+basitol@users.noreply.github.com>
2022-04-26 15:37:23 +0100

    build: logo and text

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -5,10 +5,18 @@ import bg from "../../assets/burger.png";
 const HomePage = () => {
   return (
     <>
-      <section className="flex">
-        <div></div>
-        <div>
-          <img src={bg} />
+      <section className="flex home">
+        <div className="w-50">
+          <h1 className="heading">
+            Be The Fastest In Delivering Your <span>Food</span>
+          </h1>
+          <p>
+            We cook and deliver the tastiest food right away to your designated
+            location
+          </p>
+        </div>
+        <div className="hero w-50">
+          <img src={bg} alt="burger" />
         </div>
       </section>
     </>
