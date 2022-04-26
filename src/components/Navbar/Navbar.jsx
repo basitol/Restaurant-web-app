8d5bfe5e60370ac747fbd683f7175a23d6347067
basitol <41623944+basitol@users.noreply.github.com>
2022-04-26 15:29:56 +0100

    Add: logo image

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../../assets/Burger King.png";
 import "./Navbar.css";
 
 const Navbar = () => {
   return (
     <>
       <section className="flex flex-jc-sb navbar">
-        <div className="logo">Burger Kings</div>
+        <div className="logo">
+          <img src={logo} alt="logo" />
+        </div>
         <nav className="navlinks close flex">
           <ul className="flex">
             <li>
