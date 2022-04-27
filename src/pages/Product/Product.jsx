753c0ea03e1c2ed990ab53e21eac54daf47d42e3
basitol <41623944+basitol@users.noreply.github.com>
2022-04-27 14:13:54 +0100

    build: product and added props

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,10 +1,36 @@
 import React from "react";
 import "./Product.css";
+import { ProductCard } from "../../components";
+import cake from "../../assets/cake.png";
+import cheese from "../../assets/cheese.png";
 
 const Product = () => {
   return (
     <>
-      <section>Product</section>
+      <section className="flex flex-jc-sb">
+        <ProductCard
+          head="Donut"
+          ingredients={
+            "milk, egg, butter, baking powder, sugar, flour, chocolate sauce"
+          }
+          src={cake}
+          price={35}
+        />
+        <ProductCard
+          head="Cheese Burger"
+          ingredients={
+            "tomato, mozzarella, meat, lettuse, cheese, onion, pickled cucumber, paprika"
+          }
+          src={cheese}
+        />
+        <ProductCard
+          head="Mushroom Pizza"
+          ingredients={
+            "mushroom, tomato, mozzarella, chili pepper, olive, ham, tomato paste, onion, paprika"
+          }
+          src={cake}
+        />
+      </section>
     </>
   );
 };
